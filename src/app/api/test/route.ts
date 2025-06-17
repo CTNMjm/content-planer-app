7fce10c2c52cc30dd616185dd44fddb725027f1f
CTNMjm <j.mass@ctnm.de>
2025-06-17 12:18:08 +0200

    Fix database schema issue - remove password field from test route

--- src/app/api/test/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
-
-export async function GET() {
-  const users = await prisma.user.findMany() // wenn du ein User-Modell hast!
-  return NextResponse.json(users)
-}
