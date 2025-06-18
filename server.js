c45107217a1aad81d92304ecc320e60de1738f49
CTNMjm <j.mass@ctnm.de>
2025-06-18 05:56:05 +0200

    fix: Add server.js wrapper for Coolify deployment - 20250618_055605

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 console.log("Starting Next.js application...");
-require("child_process").spawn("npm", ["run", "start"], {
-  stdio: "inherit",
-  env: process.env
+// Wrapper für Coolify Deploymentpm", ["run", "start"], {
+// Leitet zu Next.js Standard-Start um
+console.log('Starting Next.js application...');
 });
+// Führe den normalen Next.js Start ausrequire('child_process').spawn('npm', ['run', 'start'], {  stdio: 'inherit',  env: process.env});
